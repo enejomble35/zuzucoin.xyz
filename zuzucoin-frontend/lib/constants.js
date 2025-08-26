677d4ceeabfe84472963b5f8029be550b1c2b5b5
enejomble35 <memedsener@gmail.com>
2025-08-26 15:00:15 +0300

    Update constants.js

--- zuzucoin-frontend/lib/constants.js
+++ zuzucoin-frontend/lib/constants.js
@@ -1,4 +1,3 @@
 // lib/constants.js
 export const ZUZU_COLLECTION_ADDRESS =
-  "0xFC1F2f35c20eBF86eBac74dBF6Aaf1dEa3bB277F";
-export const ZUZU_CHAIN_ID = 137; // Polygon
+  "0xFC1F2f35c20eBF86eBac74dBF6Aaf1dEa3bB277F"; // Polygon (137) Edition Drop / ERC1155
