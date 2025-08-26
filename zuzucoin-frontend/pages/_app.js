a08dd91303636b7f0186f98514e98abaac4e3977
enejomble35 <memedsener@gmail.com>
2025-08-26 07:32:09 +0300

    Update _app.js

--- zuzucoin-frontend/pages/_app.js
+++ zuzucoin-frontend/pages/_app.js
@@ -1,1 +1,10 @@
+import "../styles/globals.css";
+import ThirdwebProviderWrapper from "../components/ThirdwebProviderWrapper";
 
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <ThirdwebProviderWrapper>
+      <Component {...pageProps} />
+    </ThirdwebProviderWrapper>
+  );
+}
