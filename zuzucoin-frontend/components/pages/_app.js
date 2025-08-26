8a5e6338c92faaad81d4f99d09bd18bb9ad030f7
enejomble35 <memedsener@gmail.com>
2025-08-26 06:38:12 +0300

    Update _app.js

--- zuzucoin-frontend/components/pages/_app.js
+++ zuzucoin-frontend/components/pages/_app.js
@@ -1,1 +1,12 @@
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import "../styles/globals.css";
 
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThirdwebProvider activeChain="polygon">
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
