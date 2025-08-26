0dd00f1ede95110fcccca84c7641043ba6f13a93
enejomble35 <memedsener@gmail.com>
2025-08-26 07:32:35 +0300

    Update index.js

--- zuzucoin-frontend/pages/index.js
+++ zuzucoin-frontend/pages/index.js
@@ -1,1 +1,14 @@
+import Gallery from "../components/Gallery";
 
+export default function Home() {
+  return (
+    <main style={{ padding: 24, maxWidth: 1200, margin: "0 auto", color: "#fff", background: "#000" }}>
+      <h1 style={{ fontSize: 32, marginBottom: 16 }}>ZUZU NFT Koleksiyonu</h1>
+      <p style={{ marginBottom: 24, opacity: 0.85 }}>
+        Koleksiyondaki NFT’lerimiz aşağıda listelenir. Stake / Mystery Box entegrasyonu
+        ile nadirlik ödülleri yakında burada!
+      </p>
+      <Gallery />
+    </main>
+  );
+}
