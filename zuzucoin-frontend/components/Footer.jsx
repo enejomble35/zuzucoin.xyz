1611a4936d38acf2ee1e9a43b8e87d58f0d4b283
enejomble35 <memedsener@gmail.com>
2025-08-26 20:04:48 +0300

    Create Footer.jsx

---
+++ zuzucoin-frontend/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="footer">
+      © {new Date().getFullYear()} ZUZU • Roadmap • Tokenomics • Stake • Mystery Box • Tap-to-Earn
+    </div>
+  );
+}
