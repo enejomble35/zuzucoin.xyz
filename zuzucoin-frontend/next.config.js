82fb0f780be657e207e24259d19c060fff34c751
enejomble35 <memedsener@gmail.com>
2025-08-26 17:26:12 +0300

    Update next.config.js

--- zuzucoin-frontend/next.config.js
+++ zuzucoin-frontend/next.config.js
@@ -1,14 +1,14 @@
+// zuzucoin-frontend/next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "ipfs.io" },
-      { protocol: "https", hostname: "nftstorage.link" },
       { protocol: "https", hostname: "gateway.ipfscdn.io" },
+      { protocol: "https", hostname: "nftstorage.link" },
       { protocol: "https", hostname: "cloudflare-ipfs.com" },
+      { protocol: "https", hostname: "arweave.net" },
     ],
   },
 };
-
 module.exports = nextConfig;
