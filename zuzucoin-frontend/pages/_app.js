93cb97bb48ad3dc8cbd405c59dd2bb9a3af14509
enejomble35 <memedsener@gmail.com>
2025-08-26 20:01:40 +0300

    Update _app.js

--- zuzucoin-frontend/pages/_app.js
+++ zuzucoin-frontend/pages/_app.js
@@ -1,14 +1,15 @@
 import "../styles/globals.css";
-import { ThirdwebProvider, metamaskWallet, walletConnect, coinbaseWallet } from "@thirdweb-dev/react";
-import { Polygon } from "@thirdweb-dev/chains";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { defineChain } from "@thirdweb-dev/chains";
+import { CHAIN_ID, THIRDWEB_CLIENT_ID } from "../lib/constants";
+
+const activeChain = defineChain(CHAIN_ID || 137); // Polygon
 
 export default function App({ Component, pageProps }) {
   return (
     <ThirdwebProvider
-      clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
-      activeChain={Polygon}
-      supportedWallets={[metamaskWallet(), walletConnect(), coinbaseWallet()]}
-      theme="dark"
+      activeChain={activeChain}
+      clientId={THIRDWEB_CLIENT_ID || undefined}
     >
       <Component {...pageProps} />
     </ThirdwebProvider>
