efda30a9c363205174d4b9ea5555b7e892e2dd16
enejomble35 <memedsener@gmail.com>
2025-08-26 16:38:48 +0300

    Update ThirdwebProviderWrapper.jsx

--- zuzucoin-frontend/components/ThirdwebProviderWrapper.jsx
+++ zuzucoin-frontend/components/ThirdwebProviderWrapper.jsx
@@ -1,14 +1,18 @@
-// components/ThirdwebProviderWrapper.jsx
+"use client";
+import dynamic from "next/dynamic";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { Polygon } from "@thirdweb-dev/chains";
 
-export default function ThirdwebProviderWrapper({ children }) {
+function TWProvider({ children }) {
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
-      activeChain={Polygon} // 137
+      activeChain={Polygon}
     >
       {children}
     </ThirdwebProvider>
   );
 }
+
+// SSR'i kapatıyoruz. (window is not defined hatasını engeller)
+export default dynamic(() => Promise.resolve(TWProvider), { ssr: false });
