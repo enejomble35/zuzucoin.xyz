173684efc4e257d2217ba2779e1fa76f5c759f0f
enejomble35 <memedsener@gmail.com>
2025-08-26 07:30:39 +0300

    Update constants.js

--- zuzucoin-frontend/lib/constants.js
+++ zuzucoin-frontend/lib/constants.js
@@ -1,1 +1,6 @@
+// Koleksiyon adresini tek yerden yönetelim:
+export const ZUZU_COLLECTION_ADDRESS =
+  "0xFC1F2f35c20eBF86eBac74dBF6Aaf1dEa3bB277F";
 
+// Zincir seçimi: Polygon (POL)
+export const ACTIVE_CHAIN = "polygon";
