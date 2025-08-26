f17276ac5c8bc49c06daca12b7866e2b4a04a2c4
enejomble35 <memedsener@gmail.com>
2025-08-26 07:31:11 +0300

    Update ThirdwebProviderWrapper.jsx

--- zuzucoin-frontend/components/ThirdwebProviderWrapper.jsx
+++ zuzucoin-frontend/components/ThirdwebProviderWrapper.jsx
@@ -1,1 +1,12 @@
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ACTIVE_CHAIN } from "../lib/constants";
 
+export default function ThirdwebProviderWrapper({ children }) {
+  const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID;
+
+  return (
+    <ThirdwebProvider clientId={clientId} activeChain={ACTIVE_CHAIN}>
+      {children}
+    </ThirdwebProvider>
+  );
+}
