115d35a31e6121a61e5cfe28aed00d16a81a157f
enejomble35 <memedsener@gmail.com>
2025-08-26 19:57:31 +0300

    Update next.config.js

--- zuzucoin-frontend/next.config.js
+++ zuzucoin-frontend/next.config.js
@@ -2,16 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "ipfs.io" },
-      { protocol: "https", hostname: "nftstorage.link" },
-      { protocol: "https", hostname: "cloudflare-ipfs.com" },
-      { protocol: "https", hostname: "gateway.pinata.cloud" },
-      { protocol: "https", hostname: "*.ipfs.dweb.link" },
-      { protocol: "https", hostname: "thirdweb-dev.mypinata.cloud" },
-    ],
+    domains: ["ipfs.thirdwebcdn.com", "gateway.ipfscdn.io", "nft-cdn.alchemy.com"],
   },
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
 };
+
 module.exports = nextConfig;
