8bdb52cdd66348767307e86be449fe39182bd846
enejomble35 <memedsener@gmail.com>
2025-08-26 17:18:14 +0300

    Create ClientOnly.jsx

---
+++ zuzucoin-frontend/components/ClientOnly.jsx
@@ -0,0 +1,10 @@
+// zuzucoin-frontend/components/ClientOnly.jsx
+"use client";
+import { useEffect, useState } from "react";
+
+export default function ClientOnly({ children, fallback = null }) {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return fallback;
+  return children;
+}
