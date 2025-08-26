2183cee71f51a3db118f9d05aa0c826dc5c00a9b
enejomble35 <memedsener@gmail.com>
2025-08-26 16:39:49 +0300

    Update constants.js

--- zuzucoin-frontend/lib/constants.js
+++ zuzucoin-frontend/lib/constants.js
@@ -1,6 +1,2 @@
-// lib/constants.js
-export const ZUZU_COLLECTION_ADDRESS =
-  "0xFC1F2f35c20eBF86eBac74dBF6Aaf1dEa3bB277F";
-
-// Polygon (Mainnet)
-export const ZUZU_CHAIN_ID = 137;
+export const ZUZU_COLLECTION_ADDRESS = "0xF1F2f135c20eDFB6e6Bac7d4BDF6a4a1fDEa3bB277F"; // senin adres
+export const CHAIN_ID = 137; // Polygon mainnet
