16c25587554980459a938d3867867dc5a783520e
enejomble35 <memedsener@gmail.com>
2025-08-27 12:25:31 +0300

    Update next.config.js

--- zuzucoin-frontend/next.config.js
+++ zuzucoin-frontend/next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: '**.ipfs.w3s.link' },
-      { protocol: 'https', hostname: '**.ipfs.dweb.link' },
-      { protocol: 'https', hostname: 'ipfs.io' },
-      { protocol: 'https', hostname: '**.thirdweb.com' },
-      { protocol: 'https', hostname: '**.mediarender.com' },
-      { protocol: 'https', hostname: '**.nft.storage' }
-    ]
-  }
+  images: { unoptimized: true },
 };
 module.exports = nextConfig;
