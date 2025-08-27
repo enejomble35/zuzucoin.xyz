b7d98a75eb3cde281620fb48bf887473192563aa
enejomble35 <memedsener@gmail.com>
2025-08-27 12:55:00 +0300

    Create Stake.jsx

---
+++ zuzucoin-frontend/components/Stake.jsx
@@ -0,0 +1,15 @@
+export default function Stake(){
+  return (
+    <div className="section">
+      <h2>Stake & Ödüller (Yakında)</h2>
+      <div className="card">
+        <p>Stake kontratı devreye alındığında, ZUZU stake ederek günlük ödül kazanabileceksiniz.</p>
+        <ul className="list">
+          <li>APR: Dinamik, havuz büyüklüğüne göre</li>
+          <li>Kilitsiz / kilitli seçenekleri</li>
+          <li>VIP seviyeleri ve bonuslar</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
