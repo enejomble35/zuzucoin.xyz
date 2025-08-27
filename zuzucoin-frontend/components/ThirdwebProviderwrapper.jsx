336b2d7c15c6c098dfead47a8c08a80ef3dcb8e1
enejomble35 <memedsener@gmail.com>
2025-08-27 12:26:49 +0300

    Update ThirdwebProviderwrapper.jsx

--- zuzucoin-frontend/components/ThirdwebProviderwrapper.jsx
+++ zuzucoin-frontend/components/ThirdwebProviderwrapper.jsx
@@ -1,1 +1,12 @@
+"use client";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Polygon } from "@thirdweb-dev/chains";
 
+export default function ThirdwebProviderWrapper({ children }) {
+  const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "";
+  return (
+    <ThirdwebProvider clientId={clientId} activeChain={Polygon}>
+      {children}
+    </ThirdwebProvider>
+  );
+}
