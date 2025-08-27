ee9627023e97834b9e075853ea7808bc0d87596d
enejomble35 <memedsener@gmail.com>
2025-08-27 21:34:22 +0300

    Delete zuzucoin-frontend directory

--- zuzucoin-frontend/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: { unoptimized: true }, // public içinden görüntüleri direkt kullan
-};
-
-module.exports = nextConfig;
--- zuzucoin-frontend/pages/nft.js
+++
@@ -1,8 +0,0 @@
-import { ThirdwebSDK } from "@thirdweb-dev/sdk";
-
-const sdk = new ThirdwebSDK("polygon"); // Chain ID: 137
-const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
-
-export const getNFTModule = () => {
-  return sdk.getContract(contractAddress, "nft-collection");
-};
