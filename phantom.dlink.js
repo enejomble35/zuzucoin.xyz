921c05df6cfcb7b86a5846836960ba33ff16753f
enejomble35 <memedsener@gmail.com>
2025-09-09 16:13:39 +0300

    Delete phantom.dlink.js

--- phantom.dlink.js
+++
@@ -1,5 +0,0 @@
-// Phantom deeplink: wallet içi browser'da siteyi #connect=phantom ile aç
-window.__phantomBrowseURL = function(_, APP_ORIGIN){
-  const base = (APP_ORIGIN || (location.origin));
-  return `https://phantom.app/ul/browse/${encodeURIComponent(base+'/#connect=phantom')}`;
-};
