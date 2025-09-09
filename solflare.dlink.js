7d40b49f14c096344c11d76baa6f8170d0bbb9d8
enejomble35 <memedsener@gmail.com>
2025-09-09 16:13:52 +0300

    Delete solflare.dlink.js

--- solflare.dlink.js
+++
@@ -1,4 +0,0 @@
-window.__solflareBrowseURL = function(APP_ORIGIN){
-  const base = APP_ORIGIN || location.origin;
-  return `https://solflare.com/ul/v1/browse/${encodeURIComponent(base+'/#connect=solflare')}`;
-};
