97b64fe76419fd012a242492478638c2efa1a72c
enejomble35 <memedsener@gmail.com>
2025-09-09 16:14:04 +0300

    Delete backpack.dlink.js

--- backpack.dlink.js
+++
@@ -1,4 +0,0 @@
-window.__backpackBrowseURL = function(APP_ORIGIN){
-  const base = APP_ORIGIN || location.origin;
-  return `https://www.backpack.app/ul/browse/${encodeURIComponent(base+'/#connect=backpack')}`;
-};
